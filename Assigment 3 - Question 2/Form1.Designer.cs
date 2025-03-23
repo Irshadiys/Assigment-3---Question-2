78fee9430ce10d40b075d7aea634656d60f89987
Irshad <K00297724@student.tus.ie>
2025-03-23 15:02:57 +0000

    Second commit with design, no code

--- Assigment 3 - Question 2/Form1.Designer.cs
+++ Assigment 3 - Question 2/Form1.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            btnGuess = new Button();
+            txtGuess = new TextBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(224, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Please select the number between 1-1000";
+            // 
+            // btnGuess
+            // 
+            btnGuess.Location = new Point(47, 112);
+            btnGuess.Name = "btnGuess";
+            btnGuess.Size = new Size(75, 23);
+            btnGuess.TabIndex = 1;
+            btnGuess.Text = "Guess";
+            btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // txtGuess
+            // 
+            txtGuess.Location = new Point(171, 113);
+            txtGuess.Name = "txtGuess";
+            txtGuess.Size = new Size(100, 23);
+            txtGuess.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(330, 194);
+            Controls.Add(txtGuess);
+            Controls.Add(btnGuess);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Guess the Number";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button btnGuess;
+        private TextBox txtGuess;
     }
 }
