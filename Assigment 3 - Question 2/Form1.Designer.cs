39331527afb72a47d57303e327302019159e0ee6
Irshad <K00297724@student.tus.ie>
2025-03-29 11:28:47 +0000

    Fixed Design

--- Assigment 3 - Question 2/Form1.Designer.cs
+++ Assigment 3 - Question 2/Form1.Designer.cs
@@ -31,12 +31,15 @@
             label1 = new Label();
             btnGuess = new Button();
             txtGuess = new TextBox();
+            button1 = new Button();
+            richTextBoxAnswer = new RichTextBox();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(47, 54);
+            label1.Location = new Point(27, 32);
             label1.Name = "label1";
             label1.Size = new Size(224, 15);
             label1.TabIndex = 0;
@@ -44,29 +47,62 @@
             // 
             // btnGuess
             // 
-            btnGuess.Location = new Point(47, 112);
+            btnGuess.Location = new Point(27, 72);
             btnGuess.Name = "btnGuess";
-            btnGuess.Size = new Size(75, 23);
+            btnGuess.Size = new Size(88, 23);
             btnGuess.TabIndex = 1;
             btnGuess.Text = "Guess";
             btnGuess.UseVisualStyleBackColor = true;
             // 
             // txtGuess
             // 
-            txtGuess.Location = new Point(171, 113);
+            txtGuess.Location = new Point(151, 73);
             txtGuess.Name = "txtGuess";
             txtGuess.Size = new Size(100, 23);
             txtGuess.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.AccessibleRole = AccessibleRole.None;
+            button1.Location = new Point(27, 105);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 63);
+            button1.TabIndex = 3;
+            button1.Text = "Please Show Answer";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxAnswer
+            // 
+            richTextBoxAnswer.Location = new Point(151, 106);
+            richTextBoxAnswer.Name = "richTextBoxAnswer";
+            richTextBoxAnswer.ReadOnly = true;
+            richTextBoxAnswer.Size = new Size(275, 62);
+            richTextBoxAnswer.TabIndex = 4;
+            richTextBoxAnswer.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(257, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(330, 194);
+            ClientSize = new Size(438, 194);
+            Controls.Add(label2);
+            Controls.Add(richTextBoxAnswer);
+            Controls.Add(button1);
             Controls.Add(txtGuess);
             Controls.Add(btnGuess);
             Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Guess the Number";
             ResumeLayout(false);
             PerformLayout();
@@ -77,5 +113,8 @@
         private Label label1;
         private Button btnGuess;
         private TextBox txtGuess;
+        private Button button1;
+        private RichTextBox richTextBoxAnswer;
+        private Label label2;
     }
 }
